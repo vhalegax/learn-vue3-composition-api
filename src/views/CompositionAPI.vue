3ecda6c77f9869609426c6a81f8cba7159af5a0d
Wisnu Setyawan <stephanus.ws@gmail.com>
2021-10-03 00:50:46 +0700

    add methods to increase capacity

--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -3,6 +3,15 @@
     <h1>Learn Vue 3 Composition API</h1>
     <hr />
     Capacity : {{ capacity }}
+
+    <br />
+    <br />
+    <button @click="increaseCapacityWithSetupMetdhods()">
+      Increase Capacity With setup Methods
+    </button>
+    <button @click="increaseCapacityWithNormalMethods()">
+      Increase Capacity With normal Methods
+    </button>
   </div>
 </template>
 
@@ -11,7 +20,26 @@ import { ref } from "vue";
 export default {
   setup() {
     const capacity = ref(4);
-    return { capacity };
+
+    function increaseCapacityWithSetupMetdhods() {
+      alert("plus with setup methods");
+      capacity.value += 1;
+    }
+
+    return { capacity, increaseCapacityWithSetupMetdhods };
+  },
+
+  data() {
+    return {
+      // capacity: 1,
+    };
+  },
+
+  methods: {
+    increaseCapacityWithNormalMethods() {
+      alert("plus with normal methods");
+      this.capacity += 1;
+    },
   },
 };
 </script>
