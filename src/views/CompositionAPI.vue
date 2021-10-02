a2f3945ed3cb056157f31077e7f1f9effae8c6f7
Wisnu Setyawan <stephanus.ws@gmail.com>
2021-10-03 00:38:18 +0700

    init composition API with setup and ref

---
+++ src/views/CompositionAPI.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Learn Vue 3 Composition API</h1>
+    <hr />
+    Capacity : {{ capacity }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const capacity = ref(4);
+    return { capacity };
+  },
+};
+</script>
